da62bdddc7b06257087a85765e9296bd862199ab
Joy <joyyujiepeng@gmail.com>
2024-03-03 11:31:35 +0000

    UI: improve style of navbar

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -8,7 +8,7 @@ export default async function Posts() {
   }
 
   return (
-    <section className="mt-6 max-w-3xl mx-auto sm:mx-6">
+    <section className="mt-6 max-w-5xl mx-auto sm:mx-6">
       <h2 className="text-4xl font-bold dark:text-white/90">Blog</h2>
       <ul className="w-full list-none p-0">
         {posts.map((post) => (
